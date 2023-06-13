fc01e38a6a1ece5c5851e174f818e7a832b132d3
mgmman <115487609+mgmman@users.noreply.github.com>
2023-06-13 19:03:48 +0500

    fixes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PIL import Image
 
 palette = [' ', '.', ',', '-', '^', '+', 'r', '*', ';', '|', '/', ')', 'T', 'R', 'B', '4', '@']
+palette.reverse()
 image = Image.open('test1.png')
 image = image.convert('L')
-image = image.resize((image.size[0] // 8, image.size[1]//8))
+image = image.resize((200, 200))
 with open("output.txt", "w") as f:
-    for i in range(image.size[1]-1):
-        for j in range(image.size[0]-1):
-            pixel = image.getpixel((j, i))
+    for j in range(image.size[1]-1):
+        for i in range(image.size[0]-1):
+            pixel = image.getpixel((i, j))
             print(palette[int(pixel/(256/len(palette)))], end=' ', file=f)
         print(file=f)
