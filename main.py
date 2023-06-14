4f0938291be5a60ee502dc842d61df62d0e95def
mgmman <115487609+mgmman@users.noreply.github.com>
2023-06-14 21:20:34 +0500

    addded transition to greyscale

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from PIL import Image
 
-palette = [' ', '.', ',', '-', '^', '+', 'r', '*', ';', '|', '/', ')', 'T', 'R', 'B', '4', '@']
-palette.reverse()
-image = Image.open('test1.png')
-image = image.convert('L')
-image = image.resize((200, 200))
-
 
 class ASCIIArtConvertor:
     def __init__(self):
         self.palette = [' ', '.', ',', '-', '^', '+', 'r', '*', ';', '|', '/', ')', 'T', 'R', 'B', '4', '@']
         self.palette.reverse()
 
-    def convert_to_ascii_art(self, image, output_file):
-        with open(output_file, "w") as f:
-            for j in range(image.size[1] - 1):
-                for i in range(image.size[0] - 1):
-                    pixel = image.getpixel((i, j))
-                    print(self.palette[int(pixel / (256 / len(self.palette)))], end=' ', file=f)
-                print(file=f)
+    def convert_to_ascii_art(self, image):
+        res = ''
+        for j in range(len(image[0])):
+            for i in range(len(image)):
+                pixel = image[i][j]
+                res += self.palette[int(pixel / (256 / len(self.palette)))]
+            res += '\n'
+        return res
 
     def convert_to_greyscale(self, image: Image):
+        res = [[0] * image.size[0] for _ in range(image.size[1])]
         for j in range(image.size[1] - 1):
             for i in range(image.size[0] - 1):
                 pixel = image.getpixel((i, j))
-                image.putpixel()
+                res[i][j] = (0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2])
+        return res
+
+
+if __name__ == '__main__':
+    image = Image.open('wikipedia.png')
+    convertor = ASCIIArtConvertor()
+    with open('output.txt', 'w') as f:
+        print(convertor.convert_to_ascii_art(convertor.convert_to_greyscale(image)), file=f)
